96366beaf735b180164005a035ea1726480182b9
Wen-Tien Chang <ihower@gmail.com>
2015-05-08 09:44:45 +0800

    Fix spec json time error

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "API_V1::Products", :type => :request do
 
-  let!(:p1) { Product.create!( :name => "A", :price => 100 ) }
-  let!(:p2) { Product.create!( :name => "B", :price => 200 ) }
+Time.zone = "UTC"
+
+  before do
+    @p1 = Product.create!( :name => "A", :price => 100 )
+    @p2 = Product.create!( :name => "B", :price => 200 )
+
+    # Becaise db ignores microsecond
+    # For example:
+    # original @p1.created_at.to_json is
+    # => 2015-05-08T01:38:11.283Z
+    # after saving to DB it becomes to
+    # => 2015-05-08T01:38:11.000Z
+    @p1.reload
+    @p2.reload
+  end
 
   example "GET /api/v1/products" do
+
+
 
     get "/api/v1/products"
 
@@ -15,22 +30,22 @@ RSpec.describe "API_V1::Products", :type => :request do
       {
         data: [
           {
-            url: "http://www.example.com/api/v1/products/#{p1.id}",
-            id: p1.id,
-            name: p1.name,
-            price: p1.price,
-            description: p1.description,
-            created_at: p1.created_at,
-            updated_at: p1.updated_at
+            url: "http://www.example.com/api/v1/products/#{@p1.id}",
+            id: @p1.id,
+            name: @p1.name,
+            price: @p1.price,
+            description: @p1.description,
+            created_at: @p1.created_at,
+            updated_at: @p1.updated_at
           },
           {
-            url: "http://www.example.com/api/v1/products/#{p2.id}",
-            id: p2.id,
-            name: p2.name,
-            price: p2.price,
-            description: p2.description,
-            created_at: p2.created_at,
-            updated_at: p2.updated_at
+            url: "http://www.example.com/api/v1/products/#{@p2.id}",
+            id: @p2.id,
+            name: @p2.name,
+            price: @p2.price,
+            description: @p2.description,
+            created_at: @p2.created_at,
+            updated_at: @p2.updated_at
           }
         ],
         paging: {
